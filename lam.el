22c8868ae724b6718881ca14c0e04822fa59df27
ChanderG <mail@chandergovind.org>
2023-04-23 18:49:00 +0530

    add basic working version
    can add abbrevs, but can't remove them yet!

--- lam.el
+++ lam.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding:t -*-
+
 ;; obtained from the "Buffer local abbrevs" section from https://www.emacswiki.org/emacs/AbbrevMode
 (defun set-local-abbrevs (abbrevs)
   "Add ABBREVS to `local-abbrev-table' and make it buffer local.
@@ -16,35 +18,23 @@
         (cl-third abbrev)))
     (setq-local local-abbrev-table (eval tblsym))))
 
-;; Advice approach is not working
-; not to mention - it seems to fire a lot!!
-
-;; (advice-add 'select-window :before 'lam-update)
-;; (advice-remove 'select-window 'lam-update)
-
-;; (defun lam-update (window &optional norecord)
-;;   "Called when window focus is changed. Advice added to select-window."
-;;   (message "Entering lam-update: " (buffer-name (window-buffer window)))
-;;   (if (eq (buffer-name) "*lam*")
-;;       (message "Will update Local Abbrev Manager.")))
-
-;; Similarly hooking onto 'after-change-functions is also very wasteful
-
-;; (defun lam/reload (beg end pre)
-;;   (message "Hi"))
-
-;; But - this is the best option so far!
-(defun lam/reload1 (arg)
-  (message "Win Change"))
+; the third arg is not used - only for the hook function which is a window
+(defun lam/reload (lambuffer sourcebuffer arg)
+  (with-current-buffer lambuffer
+    (let* ((buf (buffer-substring-no-properties (point-min) (point-max)))
+	   (at (read (concat "(" buf ")"))))
+      (with-current-buffer sourcebuffer
+	(set-local-abbrevs at)))))
 
 (defun lam/open ()
   (interactive)
   (abbrev-mode)
-  (let ((lambuffer (get-buffer-create "*lam*")))
+  (let ((lambuffer (get-buffer-create "*lam*"))
+        (sourcebuffer (current-buffer)))
     (display-buffer-in-side-window lambuffer nil)
     (with-current-buffer lambuffer
-        ; to make the hook local to buffer
-      (add-hook 'window-selection-change-functions 'lam/reload1 0 t)
+      ; to make the hook local to buffer
+      (add-hook 'window-selection-change-functions (apply-partially #'lam/reload lambuffer sourcebuffer) 0 t)
       )))
 
 (provide 'lam)
